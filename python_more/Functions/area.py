e1aabdacb8432db966663b7053a5cfa8c76480fe
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-19 07:32:29 +0300

    Created code for calculating area of a rectangle

---
+++ python_more/Functions/area.py
@@ -0,0 +1,9 @@
+def rectangle_area():
+    length = float(input("Enter length: "))
+    width = float(input("Enter width: "))
+    
+    area = length * width
+    print("Area of the rectangle is:", area)
+
+rectangle_area()
+# This function calculates the area of a rectangle based on user input.
