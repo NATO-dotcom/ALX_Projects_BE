c2278b66505197f4f2008c10398c3f4b74aa39f3
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-19 07:31:28 +0300

    Created code for testing whether a number is even or odd

---
+++ python_more/Functions/Test_odd_even.py
@@ -0,0 +1,10 @@
+def check_even_odd():
+    number = int(input("Enter a number: "))
+    
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
+
+check_even_odd()
+# This function checks if a number is even or odd and prints the result.5
