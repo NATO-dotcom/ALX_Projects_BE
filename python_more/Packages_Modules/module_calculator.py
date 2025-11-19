225b046553c04e3b9fba7469d42703b9a577d51e
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-19 08:22:01 +0300

    Exploring packages and modules in python

---
+++ python_more/Packages_Modules/module_calculator.py
@@ -0,0 +1,19 @@
+# calculator.py
+#user input
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+# functions for operations
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
