be3334a5e8371b32ab07781bd9ac0e99f8ccb6d8
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-19 07:33:22 +0300

    Created code for greeting the user

---
+++ python_more/Functions/greet.py
@@ -0,0 +1,7 @@
+def greet_user():
+    name = input("Enter your name: ")
+    message = f"Hello, {name}! Welcome!"
+    print(message)
+
+greet_user()
+# This function greets the user by their name.
