5435f60097112fed325c66b7ffae7ea6d6b9c999
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-19 08:53:24 +0300

    Exploring namespace in python

---
+++ python_more/variable_scope/namespace.py
@@ -0,0 +1,11 @@
+def count_numbers():
+    count = 5   # count for counting purpose
+    print("Count inside count_numbers():", count)
+
+def log_message():
+    count = "Logged successfully"   # same name, different meaning
+    print("Count inside log_message():", count)
+
+count_numbers()
+log_message()
+# This script demonstrates the use of variable namespaces in Python.
